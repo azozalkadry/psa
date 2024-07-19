fb298bcef6381d2a5892aa89c2010499b319af3d
azozalkadry <azozalkadry@gmail.com>
2024-07-20 02:48:41 +0530

    Refactor(psa): Refactoring for PSA App and resolved Conflicting

--- psa/psa_registration/doctype/request_supervisor/request_supervisor.py
+++ psa/psa_registration/doctype/request_supervisor/request_supervisor.py
@@ -3,11 +3,5 @@
 
 # import frappe
 from frappe.model.document import Document
-
-
-<<<<<<<< HEAD:psa/psa_registration/doctype/request_co_supervisor/request_co_supervisor.py
-class RequestCoSupervisor(Document):
-========
 class RequestSupervisor(Document):
->>>>>>>> 705536fd482b0e95405c5df7da733093ceef1de0:psa/psa_registration/doctype/request_supervisor/request_supervisor.py
 	pass
--- psa/psa_registration/doctype/student_research/student_research.js
+++ psa/psa_registration/doctype/student_research/student_research.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2024, Sana'a university and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Student Research", {
-// 	refresh(frm) {
+frappe.ui.form.on("Student Research", {
+	validate: function(frm) {
+        var ResearchTitleArabic = frm.doc.research_title_arabic;
+        
+        // Check if the Research Title Arabic field contains only Arabic characters, numbers, and symbols
+        var isArabic = /^[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDCF\uFDF0-\uFDFF\uFE70-\uFEFF\d\s!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]*$/.test(ResearchTitleArabic);
 
-// 	},
-// });
+        if (!isArabic) {
+            frappe.msgprint('<span style = "color:red;">The Research Title Arabic field should only contain Arabic characters, numbers, and symbols.</span>');
+            frappe.validated = false;
+        }
+
+        var ResearchTitleEnglish = frm.doc.research_title_english;
+        // Check if the Research Title English field contains only English letters, numbers, and symbols
+        var isEnglish = /^[a-zA-Z0-9\s!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]*$/.test(ResearchTitleEnglish);
+        
+        if (!isEnglish) {
+            frappe.msgprint('<span style="color:red;">The Research Title English field should only contain English characters, numbers, and symbols.</span>');
+            frappe.validated = false;
+        }
+    }
+});
